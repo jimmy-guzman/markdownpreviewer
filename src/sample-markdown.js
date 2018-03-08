1304011ef3a4e32cd1dcfb6b48f9eb7b3c009c80
Jimmy Guzman <jimmy.guzman@outlook.com>
2018-03-08 15:16:17 -0600

    changed sample markdown text and mixed max-width

--- src/sample-markdown.js
+++ src/sample-markdown.js
@@ -1,9 +1,8 @@
-const sampleMarkdown = `Headers
+const sampleMarkdown = `This is a paragraph
 # This is an <h1> tag
 ## This is an <h2> tag
 ###### This is an <h6> tag
 
-Emphasis
 *This text will be italic*
 _This will also be italic_
 
@@ -12,33 +11,26 @@ __This will also be bold__
 
 _You **can** combine them_
 
-
-Lists
-Unordered
+unordered
 * Item 1
 * Item 2
   * Item 2a
   * Item 2b
 
-Ordered
+ordered
 1. Item 1
 1. Item 2
 1. Item 3
     1. Item 3a
     1. Item 3b
 
-
-Links
 http://github.com - automatic!
 [GitHub](http://github.com)
 
 Blockquotes
-As Kanye West said:
 
 > We're living the future so
 > the present is our past.
-
-
 `;
 
 export default sampleMarkdown;
